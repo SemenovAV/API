10b72712152d0e785b4c203e08a8aac854355483
Aleksey Semenov <7.on.off@bk.ru>
2020-11-08 07:16:40 +0300

    fix: postgresql

--- src/config/settings.py
+++ src/config/settings.py
@@ -82,6 +82,17 @@ DATABASES = {
         'PORT': os.environ.get('POSTGRES_PORT'),
     },
 }
+if os.environ.get('GITHUB_WORKFLOW'):
+    DATABASES = {
+        'default': {
+           'ENGINE': 'django.db.backends.postgresql',
+           'NAME': 'github_actions',
+           'USER': 'postgres',
+           'PASSWORD': 'postgres',
+           'HOST': '127.0.0.1',
+           'PORT': '5432',
+        }
+    }
 
 
 # Password validation
