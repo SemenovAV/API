a5c97376b4f41d5741367707571ebc0cb224c364
Aleksey Semenov <7.on.off@bk.ru>
2020-11-08 17:28:12 +0300

    fix: formatting

--- src/config/settings.py
+++ src/config/settings.py
@@ -88,7 +88,7 @@ if os.environ.get('GITHUB_WORKFLOW'):
             'PASSWORD': 'postgres',
             'HOST': '127.0.0.1',
             'PORT': '5432',
-        }
+        },
     }
 
 # Password validation
