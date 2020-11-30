8b8e4e31f4e90a0e2e4cd84b0e81237d221c9d24
Aleksey Semenov <7.on.off@bk.ru>
2020-11-30 22:52:11 +0300

    style: fix codestyle

--- src/users/tests.py
+++ src/users/tests.py
@@ -1,13 +1,21 @@
+import secrets
+import string
+
 import pytest
 
 from django.contrib.auth import get_user_model
 from django.db import IntegrityError
 
 
+def get_password(length):
+    return ''.join((secrets.choice(string.ascii_letters) for i in range(length)))
+
+
 @pytest.mark.django_db
 def test_create_user():
     User = get_user_model()
-    user = User.objects.create_user(email='normal@user.com', first_name="User", last_name="Userson", password='foo')
+    password = get_password(8)
+    user = User.objects.create_user(email='normal@user.com', first_name="User", last_name="Userson", password=password)
     assert user.email == 'normal@user.com'
     assert user.is_active
     assert not user.is_staff
@@ -20,16 +28,17 @@ def test_create_user():
         pass
     with pytest.raises(TypeError):
         User.objects.create_user()
-    with pytest.raises(ValueError):
+    with pytest.raises(ValueError, match='The given email must be set'):
         User.objects.create_user(email='')
-    with pytest.raises(ValueError):
-        User.objects.create_user(email='', first_name="User", last_name="Userson", password="foo")
+    with pytest.raises(ValueError, match='The given email must be set'):
+        User.objects.create_user(email='', first_name="User", last_name="Userson", password=password)
 
 
 @pytest.mark.django_db
 def test_create_superuser():
     User = get_user_model()
-    admin_user = User.objects.create_superuser('super@user.com', 'foo', first_name='User', last_name='Userson')
+    password = get_password(8)
+    admin_user = User.objects.create_superuser('super@user.com', password, first_name='User', last_name='Userson')
     assert admin_user.email == 'super@user.com'
     assert admin_user.first_name == 'User'
     assert admin_user.is_active
@@ -43,4 +52,4 @@ def test_create_superuser():
         pass
     with pytest.raises(IntegrityError):
         User.objects.create_superuser(
-            email='super@user.com', password='foo', is_superuser=False)
+            email='super@user.com', password=password, is_superuser=False)
